7e4b95406d280551eeff66f5b0a79fecf425535f
Jake Lim <jakelim070@gmail.com>
2024-03-15 14:08:32 +0800

    add installers. add pip install ps1

---
+++ pipinstalls.ps1
@@ -0,0 +1,52 @@
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install seaborn
+pip install pyarrow
+pip install bokeh
+
+pip install cryptography
+pip install tokenizers
+pip install pylint
+pip install pyflakes
+pip install black
+pip install ruff
+pip install markdown
+pip install pytest
+pip install bcrypt
+pip install pyjwt
+pip install boto3
+pip install pyyaml
+pip install tomlkit
+pip install watchdog
+pip install pyotp
+pip install lxml
+pip install natsort
+pip install py7zr
+pip install dotenv-python
+pip install pyinstaller
+pip install click
+pip install awscli
+pip install h5py
+
+pip install anyio
+pip install customtkinter
+pip install kivy
+pip install python-pptx
+pip install openpyxl
+
+pip install opencv-python
+pip install sqlalchemy
+pip install pymssql
+pip install psycopg
+pip install psycopg2
+pip install scikit-learn
+
+pip install flask
+pip install fastapi
+pip install dash
+pip install jupyterlab
+pip install plotly
+pip install ipywidgets
+pip install beautifulsoup4
+pip install selenium
