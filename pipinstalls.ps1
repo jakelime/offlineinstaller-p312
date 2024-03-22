c5bd1e940f3108569c9bcb232403852205439391
Jake Lim <jakelim070@gmail.com>
2024-03-22 10:51:49 +0800

    update django. add more libs

---
+++ pipinstalls.ps1
@@ -0,0 +1,59 @@
+pip install pylint
+pip install black
+pip install ruff
+pip install python-dotenv
+pip install pytest
+pip install pyyaml
+pip install tomlkit
+pip install GitPython
+pip install sphinx
+pip install sphinx-rtd-theme
+pip install pyinstaller
+pip install cryptography
+pip install pycryptodome
+pip install pyotp
+pip install anyio
+pip install schedule
+pip install watchdog
+pip install boto3
+
+pip install pandas
+pip install statsmodels
+pip install matplotlib
+pip install seaborn
+pip install scipy
+pip install opencv-python
+pip install plotly
+pip install dash
+
+pip install pypdf
+pip install tables
+pip install tabula-py
+pip install python-pptx
+pip install openpyxl
+pip install xlrd
+
+pip install pyodbc
+pip install sqlalchemy
+pip install psycopg2
+pip install pymssql
+
+pip install flask
+pip install fastapi
+pip install requests-oauthlib
+pip install pyjwt
+pip install uvicorn
+pip install gunicorn
+pip install "connexion[swagger-ui]"
+pip install waitress
+pip install mod-wsgi
+
+pip install Django<5
+pip install djlint
+pip install django-crispy-forms
+pip install django-bootstrap5
+pip install crispy-bootstrap5
+pip install django-tables2
+pip install django-filter
+
+
